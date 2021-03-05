c146a9dcb64aab76baa28cf1190cef1a1ca75ee0
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-03-05 16:40:36 -0700

    cleanup

--- 3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
+++ 3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
@@ -15,23 +15,28 @@ class LinkList {
         
     func addFront(_ data: Int) {
     }
-        
+
+    func getFirst() -> Int {
+        return 0
+    }
+
     func addBack(_ data: Int) {
     }
-    
+
+    func getLast() -> Int {
+        return 0
+    }
+
     func insert(position: Int, data: Int) {
     }
     
     func deleteFirst() {
     }
-    
+
     func deleteLast() {
     }
-    
-    func delete(data: Int) {
-    }
-
-    func printLinkedList() {
+        
+    func delete(at position: Int) {
     }
     
     var isEmpty: Bool {
@@ -40,4 +45,20 @@ class LinkList {
     
     func clear() {
     }
+    
+    func printLinkedList() {
+        if head == nil { return }
+        
+        var result = [Int]()
+        var node = head
+        result.append(node!.data)
+        
+        while node?.next != nil {
+            result.append(node!.next!.data)
+            node = node?.next
+        }
+        
+        print(result)
+    }
+
 }
