37ae144c075fe7bc8b63e56581d38d0c78de5cdb
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-03-18 16:51:00 -0600

    cleanup

--- 5-StacksQueues/Q2-ReverseAString-Answer.playground/Contents.swift
+++ 5-StacksQueues/Q2-ReverseAString-Answer.playground/Contents.swift
@@ -7,15 +7,6 @@ import UIKit
  using a stack.
  */
 
-/*
-The followings are the steps to reversing a String using Stack:
-String to char[].
-Create a Stack.
-Push all characters, one-by-one.
-Then Pop all characters, one by one and put into the char[].
-Finally, convert to the String.
-*/
-
 func solution(_ text: String) -> String {
     var chars = Array(text)
     
---
+++ 5-StacksQueues/Q2-ReverseAString-Question.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// https://www.fullstack.cafe/interview-questions/stacks
+
+/*
+ Giving a String, write a function that reserves the String
+ using a stack.
+ */
+
+func solution(_ text: String) -> String {
+    // Do your work here... 
+    return ""
+}
+
+solution("abc") // bca
+solution("Would you like to play a game?")
