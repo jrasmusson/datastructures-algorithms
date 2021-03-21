31528db9e4af25930698ec2ebccd0e42d9fc49f4
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-03-21 08:22:08 -0600

    cleanup

---
+++ 5-StacksQueues/Q3-BalancedBrackets-Answer.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+/*
+ Balanced brackets
+ https://www.hackerrank.com/challenges/balanced-brackets/problem
+ 
+ A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+ Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+ A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+ By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+ It contains no unmatched brackets.
+ The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+ Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, return YES. Otherwise, return NO.
+ 
+ */
+
+
+
+func isBalanced(s: String) -> String {
+
+    var st = [Character]()
+    
+    for c in s {
+        switch c {
+        case "{", "(", "[":
+            st.append(c)
+        case "}":
+            if (st.isEmpty || (st.last != "{")) {
+                return "NO"
+            }
+            st.popLast()
+        case ")":
+            if (st.isEmpty || (st.last != "(")) {
+                return "NO";
+            }
+            st.popLast()
+        case "]":
+            if (st.isEmpty || (st.last != "[")) {
+                return "NO";
+            }
+            st.popLast()
+        default:
+            print("breaking \(c)")
+        }
+    }
+    
+    return st.isEmpty ? "YES" : "NO"
+}
+
+isBalanced(s: "{[()]}")
+//isBalanced(s: "{}()(){}((){})({[[({({(){}{}}){}})]{({()}((())))}()]})(({}(()){[][]}){()}(({}{}))())()[](){{((){})}}()([[]])[][]()({}((([()]{})())[][[()]]())){{}}[]{()}()[][]{}([])[]{({})}{}{{}{[[]]}[]{}}{[()]}[]{(([{{[{[]}]}[{}]}]))}(){}{{}}[]((([])([{(){}[(()[]((()(){})({([]({{{[]{}}[({})()({}{([()])()()[]{}})][{[]}]{{}([]({{{(()(({}[[[{{}}]]{{[()]([[{{}([[]][([{{}}(([])[][({()}())()({}[])]{}[])]())[]]){}}[]]])([]({{[[][]{[]}[]]}}{}(){[]}))}()[]((){{}()[{[[()]]}()]}[()]{})}][]{}))())}(())}{{[]}{}}({[([{[{[[[]]]{()}[]}]{}}()((({{{{({{(){}}}[[()]()[]]())({{{[]}{{[[{{[{}]}}[][]]]([][](()(()[]){{}}))([])}}}}[{}{}])[(){{()()}{(())}()}]{(){{}[]{}[][{[]([[]()]{(){[{}[()]][{}{}]{(){}}}{[]}}{[]}[]){[]}[]}][((){}{}[[[[{{}()[([({{[[][{{()}(([[]][[[[[[[{}]][{}]]]()](())[()[][]({({[][][[]{}][]}{})}{({})([[][]({}{[]})])[([([])][[]{([])(({}))}](()[]){[[]]}({}))]}[])()]]]))([{}()()([([[{}][()]][])])][[[{}][][]({[]})][(({{()}}))]])}]]}})])]}]]]])]}}}}}})))])]})}))}}}))})))]}])))")
