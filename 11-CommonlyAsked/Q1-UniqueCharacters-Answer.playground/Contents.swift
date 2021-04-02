65243fc0adb24c5f8b0eb59dbe4bff1f4775df35
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-04-02 06:23:39 -0600

    cleanup

--- 11-CommonlyAsked/Q1-UniqueCharacters-Answer.playground/Contents.swift
+++ 11-CommonlyAsked/Q1-UniqueCharacters-Answer.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+
 /*
  _   _      _                ___ _
 | | | |_ _ (_)__ _ _  _ ___ / __| |_  __ _ _ _ ___
@@ -6,10 +7,31 @@ import UIKit
  \___/|_||_|_\__, |\_,_\___|\___|_||_\__,_|_| /__/
                 |_|
 
- */
-
-/*
- 
  Challenge: Given a string, see if you can detect whether it contains only unique chars.
  
  */
+
+/*
+ Solution:
+ 
+  There are lots of ways we could solve this:
+       - HashMap where we store every character and a boolean on whether found
+       - An array that counts the number of times a character occurs
+ */
+
+func isUnique(_ text: String) -> Bool {
+    var foundChars = [Character:Bool]()
+    let chars = Array(text)
+
+    for c in chars {
+        if foundChars[c] != nil { // Already found
+            return false
+        }
+        foundChars[c] = true
+    }
+    return true
+}
+
+isUnique("ab")
+isUnique("aa")
+isUnique("abcdefghijklmnopqrstuvwxyz")
