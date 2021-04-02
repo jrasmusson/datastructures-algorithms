4d79345d97491745e7eb9b05d9e4ef30bebe6a4b
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-04-02 08:23:51 -0600

    cleanup

--- 11-CommonlyAsked/Q5-Compressor.playground/Contents.swift
+++ 11-CommonlyAsked/Q5-Compressor.playground/Contents.swift
@@ -6,5 +6,37 @@ import UIKit
 |(__/ _ \ '  \| '_ \ '_/ -_|_-<_-</ _ \ '_|
 \___\___/_|_|_| .__/_| \___/__/__/\___/_|
               |_|
+ 
+ // Challenge: Give a string with repeating characters (i.e. "aaabb") write
+ // an algorithm that will compress the string down to the character, followed
+ // by the number of times it appears in the string (i.e "a3b2").
+ // If the compressed string is not smaller than original, return original.
+
  */
-var str = "Hello, playground"
+
+func compress(_ str: String) -> String {
+    var compressed = ""
+    var count = 0
+    let chars = Array(str)
+    
+    for i in 0..<str.count {
+        count += 1
+        // If we are at the end of our string
+        // or if next char different, append this result
+        if i + 1 >= str.count || chars[i] != chars[i + 1] {
+            compressed.append(chars[i])
+            compressed.append(String(count))
+            count = 0
+        }
+    }
+    
+    // Return the original string if shorter, or the compressed if longer
+    return compressed.count < str.count ? compressed : str
+}
+
+compress("aaabb")           // a3b2
+compress("aabb")            // aabb
+compress("ab")              // ab
+compress("abc")             // abc
+compress("zzz")             // z3
+compress("aabbaabb")        // aabbaabb (not shorter)
