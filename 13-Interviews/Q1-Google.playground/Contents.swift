b41c952ce664edb4022fcdbd020f01b19259c312
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-04-04 08:17:16 -0600

    cleanup

---
+++ 13-Interviews/Q1-Google.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+/*
+ ___                _
+/ __|___  ___  __ _| |___
+|(_ / _ \/ _ \/ _` | / -_)
+\___\___/\___/\__, |_\___|
+              |___/
+
+ // https://www.youtube.com/watch?v=XKu_SEDAykw&ab_channel=LifeatGoogle
+ 
+ Given a set of numbers, determine if there is a pair that equals a given sum.
+ 
+ */
+
+func hasPairWithSum(_ arr: [Int], _ sum: Int) -> Bool {
+    return false
+}
+
+hasPairWithSum([1, 2, 4, 9], 8)     // false
+hasPairWithSum([1, 2, 4, 4], 8)     // true
