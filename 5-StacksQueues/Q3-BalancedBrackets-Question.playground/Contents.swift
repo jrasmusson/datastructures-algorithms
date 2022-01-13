9c8edff54cdaceb892c8d6ae187d8775a496ae89
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-01-13 10:48:41 -0700

    Remove answer from balanced bracket question playground

--- 5-StacksQueues/Q3-BalancedBrackets-Question.playground/Contents.swift
+++ 5-StacksQueues/Q3-BalancedBrackets-Question.playground/Contents.swift
@@ -19,32 +19,8 @@ import UIKit
  */
 
 func isBalanced(s: String) -> String { // "{[()]}"
-    var st = [Character]()
-    
-    for c in s {
-        switch c {
-        case "{", "(", "[":
-            st.append(c)
-        case "}":
-            if (st.isEmpty || (st.last != "{")) {
-                return "NO"
-            }
-            st.popLast()
-        case ")":
-            if (st.isEmpty || (st.last != "(")) {
-                return "NO";
-            }
-            st.popLast()
-        case "]":
-            if (st.isEmpty || (st.last != "[")) {
-                return "NO";
-            }
-            st.popLast()
-        default:
-            print("breaking \(c)")
-        }
-    }
-    return st.isEmpty ? "YES" : "NO"
+    // 🕹 Game on here
+    return "NO"
 }
 
 isBalanced(s: "{[()]}") // Yes
